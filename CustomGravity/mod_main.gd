c31f0dac80caf1a6eda4536dd9ac3b95bb8d021b
banjo <luca.easley@gmail.com>
2025-02-25 14:51:39 -0500

    Initial commit

--- CustomGravity/mod_main.gd
+++ CustomGravity/mod_main.gd
@@ -1,6 +1,6 @@
 extends "res://addons/ModLoader/mod_node.gd"
 
-var gravity_setting = Setting.new("Gravity Multiplier", Setting.SETTING_FLOAT, 0.3, Vector2(0, 1))
+var gravity_setting = Setting.new("Gravity Multiplier", Setting.SETTING_FLOAT, 0.3, Vector2(0, 5))
 
 func init():
 	ModLoader.mod_log(name_pretty + " mod loaded")
@@ -13,8 +13,8 @@ func init():
 	}
 
 func _process(_delta):
-	if is_instance_valid(GameManager):
+	if is_instance_valid(GameManager.player):
 		var default_gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 		var target_gravity = default_gravity * gravity_setting.value
-		if GameManager.gravity != target_gravity:
-			GameManager.gravity = target_gravity
+		if GameManager.player.gravity != target_gravity:
+			GameManager.player.gravity = target_gravity
